2fe1da8f914c425ef25937e2c85f46bcf93115d4
Şenay Şentürk <senaysenturk@outlook.com>
2023-06-08 19:49:59 +0300

    the list has been made dynamic.

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./todo.component.css'],
 })
 export class TodoComponent {
+  items = [
+    {
+      id: 1,
+      description: 'Kahvaltı',
+      action: 'Yes',
+    },
+    {
+      id: 2,
+      description: 'Spor',
+      action: 'Yes',
+    },
+    {
+      id: 3,
+      description: 'Alışveriş',
+      action: 'Yes',
+    },
+  ];
   title = 'todo-app-angular';
 }
