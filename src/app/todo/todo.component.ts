9346564287329805d7b3c5f6b4c5eaf78d702e40
Şenay Şentürk <senaysenturk@outlook.com>
2023-06-08 17:59:02 +0300

    app design completed

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+  styleUrls: ['./todo.component.css'],
 })
 export class TodoComponent {
-
+  title = 'todo-app-angular';
 }
